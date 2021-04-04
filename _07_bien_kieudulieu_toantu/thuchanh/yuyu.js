ef01e2e3f8dd96c5adf7d6bfc9060e9456b20af4
AiYuki <khinhvu2707@gmail.com>
2021-04-04 10:38:48 +0700

    Commit bai tap bai 7 bien-kieudulieu-toantu

--- _07_bien_kieudulieu_toantu/thuchanh/yuyu.js
+++ _07_bien_kieudulieu_toantu/thuchanh/yuyu.js
@@ -1,8 +1,14 @@
 let inputWidth;
 let inputHeight;
+
+
 inputWidth = prompt("Enter the width");
 inputHeight = prompt("Enter the height");
 let width = parseInt(inputWidth);
 let height = parseInt(inputHeight);
 let area = width * height;
+document.write("width: </br>" +inputWidth);
+document.write("<br>")
+document.write("height <br>" +inputHeight);
+document.write("<br>")
 document.write("The area is: " + area);
